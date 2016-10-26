6d915e7efd217c3aa3b2262cc0100df26e4bf18f
Riady <riady.chen@yahoo.com>
2016-10-26 13:45:58 +0700

    Initialize Statistic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def index
-  	top_ten_pokemon_winner = execute_sql("")
+  	top_ten_pokemon_winner = execute_sql("select p.name, count(p.id) from pokemons p inner join pokemon_battles pb on p.id = pb.pokemon_winner_id GROUP BY p.id ORDER BY count DESC LIMIT 10")
   end
 
   private
